02ab49fb0706e897b74b79ee64da54c6f39266cd
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-04-02 15:56:37 -0300

    Add files via upload

---
+++ 02042025/02042025/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+int Number_A=0, Number_B=0;
+WriteLine("Digite dois valores");
+Number_A = Convert.ToInt32(ReadLine());
+Number_B = Convert.ToInt32(ReadLine());
+WriteLine($"Valor A: {Number_A}, Valor B: {Number_B}");
+Number_A += Number_B;
+Number_B = Number_A - Number_B;
+Number_A += -Number_B;
+WriteLine($"Valor A: {Number_A}, Valor B: {Number_B}");
+ReadKey();
