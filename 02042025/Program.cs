a2a7078fd44ecb737f2fad9e56586787452f1ce2
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-04-02 14:50:01 -0300

    Add files via upload

---
+++ 02042025/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+int Number_A, Number_B;
+
+WriteLine("Digite um valor A de 0 a 10");
+Number_A = Convert.ToInt32(ReadLine());
+WriteLine("Digite mais um valor B de 0 a 10");
+Number_B = Convert.ToInt32(ReadLine());
+WriteLine($"Valor de A: {Number_A}, Valor de B: {Number_B}");
+ReadKey();
