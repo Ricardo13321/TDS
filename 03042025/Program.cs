3489a8d75653c043dcdb4f4ecb090de1ba4b755c
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-04-03 14:45:06 -0300

    Add files via upload

---
+++ 03042025/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+using static System.Console;
+
+int a = Convert.ToInt32(ReadLine());
+
+if (a>4)
+{
+    WriteLine($"O número {a} é maior que 4!");
+} else  if (a>3)
+{
+    WriteLine($"O número {a} é maior que 3!");
+} else if (a>0)
+{
+    WriteLine($"O número {a} é maior que 0!");
+} else
+{
+    WriteLine($"O {a} é menor que 0!");
+}
+ReadLine();
