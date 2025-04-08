6fc999c809cc226c76463bbd79a3e6418da9ec0f
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-04-08 17:20:01 -0300

    For, While e coisas coloridas.
    Coisas coloridas

---
+++ 08042025/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+ConsoleColor[] Cores = (ConsoleColor[]) ConsoleColor.GetValues(typeof(ConsoleColor));
+int Number_1 = Convert.ToInt32(ReadLine());
+Random randnum = new Random();
+Clear();
+for (int i = 0; i < Number_1; i++)
+{
+    for (int x = 1; x < Cores.Length; x++)
+    {
+        ForegroundColor = Cores[x];
+        //BackgroundColor = Cores[x];
+        //WriteLine(randnum.Next(100000000, 900000000));
+        WriteLine("☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻");
+        Thread.Sleep(1000);
+    }
+}
+BackgroundColor = ConsoleColor.Black;
+ReadLine();
