80a0a41d0bf102c91e642a4f22728e1cfa6d16bc
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-04-10 16:30:56 -0300

    Add files via upload

---
+++ 10042025/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+string[] Nomes = {"Maria", "Judas", "Nilson", "Marcos", "Carlos"};
+
+Array.Sort(Nomes);
+
+foreach (var car in Nomes)
+{
+    WriteLine(car);
+}
+
+ReadLine();
