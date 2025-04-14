e7ea9f1cb596c069b8579cb99730c6654772de5d
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-04-14 15:55:56 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+List<string> NameList = new List<string> { };
+
+for (int i = 0; i < 10; i++)
+{
+    Write($"Escreva o nome: ");
+    NameList.Add(ReadLine());
+}
+WriteLine("\n####################################");
+WriteLine("              LISTA DE NOMES");
+WriteLine("####################################");
+foreach (var _loc_ in NameList)
+{
+    WriteLine(_loc_);
+}
+ReadLine();
