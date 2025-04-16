422ad6eda115926f6eb7d2719b9e08bf8ea5d0c9
JDK☕️ <95084728+Ricardo13321@users.noreply.github.com>
2025-04-16 14:56:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyNamespace
+{
+    class ManipuladorNumero
+    {
+        public int fatorial(int num)
+        {
+            int resultado;
+            if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                resultado = num;
+                for (int i = num - 1; i >= 1; i--)
+                {
+                    resultado = resultado * i;
+                }
+                return resultado;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            ManipuladorNumero n = new ManipuladorNumero();
+            WriteLine("Digite um número inteiro");
+            int numero = Convert.ToInt32(ReadLine());
+            WriteLine("O fatorial do número digitado é: {0}", n.fatorial(numero));
+            int _loc_ = 41534634165446464654564;
+            WriteLine(_loc_);
+        }
+    }
+}
